4873bacbf0655ca36bd6097d2afd78f4f54d4823
Yuzhou <yuzhou.song@novelconseil.com>
2019-06-12 17:14:44 +0200

    add get image for topic

--- back/blogsyzadele/src/main/java/com/syzadele/blogsyzadele/controller/TopicController.java
+++ back/blogsyzadele/src/main/java/com/syzadele/blogsyzadele/controller/TopicController.java
@@ -1,6 +1,7 @@
 package com.syzadele.blogsyzadele.controller;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -20,11 +21,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.syzadele.blogsyzadele.model.Post;
 import com.syzadele.blogsyzadele.model.Topic;
-import com.syzadele.blogsyzadele.model.TopicCoverPhoto;
 import com.syzadele.blogsyzadele.repository.PostRepository;
 import com.syzadele.blogsyzadele.repository.TopicRepository;
 import com.syzadele.blogsyzadele.service.ThumbnailService;
-import com.syzadele.blogsyzadele.service.TopicCoverPhotoService;
 import com.syzadele.blogsyzadele.service.UploadService;
 
 
@@ -54,29 +53,35 @@ public class TopicController {
 		if (!topicRepository.existsByName(name)) {
 			Topic t = new Topic(name, presentation);
 			String uploadPath = "images/topicImages/";
-			String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
-			File theDir = new File(realUploadPath + name);
-			System.out.println(theDir);
-
-			// if the directory does not exist, create it
-			if (!theDir.exists()) {
-			    System.out.println("creating directory: " + theDir.getName());
-			    theDir.mkdir();
-			} else {
-				System.out.println("dir already existe");
-			}
-			if (files != null) {
-				topicRepository.save(t);
-				Topic tt = topicRepository.findByName(name);
-				int id = tt.getId();
-				addCoverPhotos(files, id);
-				/*
-				List<TopicCoverPhoto> tcp = tcps.storeMultipleFile(files, t);
-				t.setCoverPhotos(tcp);
-				*/
-				return t;
-			} else {
-				return topicRepository.save(t);
+			
+			try {
+				String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
+				File theDir = new File(realUploadPath + name);
+				System.out.println(theDir);
+	
+				// if the directory does not exist, create it
+				if (!theDir.exists()) {
+				    System.out.println("creating directory: " + theDir.getName());
+				    theDir.mkdir();
+				} else {
+					System.out.println("dir already existe");
+				}
+				if (files != null) {
+					topicRepository.save(t);
+					Topic tt = topicRepository.findByName(name);
+					int id = tt.getId();
+					addCoverPhotos(files, id);
+					/*
+					List<TopicCoverPhoto> tcp = tcps.storeMultipleFile(files, t);
+					t.setCoverPhotos(tcp);
+					*/
+					return t;
+				} else {
+					return topicRepository.save(t);
+				}
+			} catch (Exception e) {
+				System.out.println("topicImages folder not found");
+				return null;
 			}
 		} else {
 			return null;
@@ -89,33 +94,30 @@ public class TopicController {
 		if (topicRepository.existsById(id)) {
 			Topic t = topicRepository.findById(id).get();
 			String uploadPath = "images/topicImages/";
-			String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
-			
-			File theDir = new File(realUploadPath + t.getName() + "/");
-			if (theDir.exists()) {
-				File[] contents = theDir.listFiles();
-				if (contents != null) {
-			        for (File f : contents) {
-			            f.delete();
-			        }
-			    }
-				theDir.delete();
-			} else {
-				System.out.println("folder not found.");
+			try {
+				String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
+				
+				File theDir = new File(realUploadPath + t.getName() + "/");
+				if (theDir.exists()) {
+					File[] contents = theDir.listFiles();
+					if (contents != null) {
+				        for (File f : contents) {
+				            f.delete();
+				        }
+				    }
+					theDir.delete();
+					topicRepository.deleteById(id);
+					return "delete successful.";
+				} else {
+					return "topic photo folder not found.";
+				}
+			} catch (Exception e) {
+				return "topicImages folder not found";
 			}
-			topicRepository.deleteById(id);
-			return "delete successful.";
 		}
 		return "Topic not found.";
 	}
 	
-	@Transactional 
-	@RequestMapping(method = RequestMethod.POST, value = "/DeleteOneByName")
-	public void deleteOneByName(@RequestParam(value="name") String name) {
-		if (topicRepository.findByName(name) != null) {
-			topicRepository.deleteByName(name);
-		}
-	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/UpdateOne")
 	public Topic update(Topic t) {
@@ -216,15 +218,19 @@ public class TopicController {
 			
 			String topicName = t.getName();
 			String uploadPath = "images/topicImages/" + topicName + "/";
-			String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
+			try {
+				String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
+				String imageUrl = uploadService.uploadImage(file, uploadPath, topicName, realUploadPath);
+		        String thumImageUrl = thumbnailService.thumbnail(file, uploadPath,topicName, realUploadPath);
 			
-			String imageUrl = uploadService.uploadImage(file, uploadPath, topicName, realUploadPath);
-	        String thumImageUrl = thumbnailService.thumbnail(file, uploadPath,topicName, realUploadPath);
-			
-	        Map <String, String> imageURLSMap = new HashMap<String, String>();
-	        imageURLSMap.put(file.getOriginalFilename(), imageUrl);
-	        imageURLSMap.put("thum_"+file.getOriginalFilename(), thumImageUrl);
-	        return imageURLSMap;
+		        Map <String, String> imageURLSMap = new HashMap<String, String>();
+		        imageURLSMap.put(file.getOriginalFilename(), imageUrl);
+		        imageURLSMap.put("thum_"+file.getOriginalFilename(), thumImageUrl);
+		        return imageURLSMap;
+			} catch (Exception e) {
+				System.out.println("Some folder is missing or the path is incorrect.");
+				return null;
+			}
 		}
 		return null;
 	}
@@ -243,105 +249,195 @@ public class TopicController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/DeleteCoverPhoto")
-	public boolean deleteCoverPhoto(@RequestParam(value="photoName") String photoName, @RequestParam(value="id") int id) {
+	public String deleteCoverPhoto(@RequestParam(value="photoName") String photoName, @RequestParam(value="id") int id) {
 		if (topicRepository.existsById(id)) {
 			Topic t = topicRepository.findById(id).get();
 			String topicName = t.getName();
-			String uploadPath = "images/topicImages/" + topicName + "/";
-			String realUploadPath = getClass().getClassLoader().getResource(uploadPath).getPath();
+			String downloadPath = "images/topicImages/";
 			
-			String filePath = realUploadPath + topicName + "_" + photoName;
-			String thumFilePath = realUploadPath + "thum_" + topicName + "_" + photoName;
-			System.out.println(filePath);
-			System.out.println(thumFilePath);
-			
-			if (new File(filePath + ".jpg").exists()) {
-				File fileToDelete = new File(filePath + ".jpg");
-				File thumFileToDelete = new File(thumFilePath + ".jpg");
-				boolean result1 = fileToDelete.delete();
-			    boolean result2 = thumFileToDelete.delete();
-			    return result1 & result2;
-			} else {
-				File fileToDelete = new File(filePath + ".PNG");
-				File thumFileToDelete = new File(thumFilePath + ".PNG");
-				boolean result1 = fileToDelete.delete();
-			    boolean result2 = thumFileToDelete.delete();
-			    return result1 & result2;
+			try {
+				String realDownLoadPath = getClass().getClassLoader().getResource(downloadPath).getPath();
+				File dir = new File(realDownLoadPath + topicName + "/");
+				if (dir.exists()) {
+					
+					File [] files = dir.listFiles(new FilenameFilter() {
+					    @Override
+					    public boolean accept(File dir, String name) {
+					        return name.contains("_" + photoName + ".");
+					    }
+					});
+					for (File f : files) {
+						f.delete();
+					}
+					return "photos deleted";
+				} else {
+					return "topic photo folder not found";
+				}
+			} catch (Exception e) {
+				return "topicImages folder not found";
 			}
 		} 
-		return false;
+		return "topic not found";
 	}
 	
-	
-	
-	/*@RequestMapping(method = RequestMethod.POST, value = "/AddCoverPhoto")
-	public Topic addCoverPhoto(@RequestParam(value="file")  MultipartFile file, @RequestParam(value="id") int id) {
+	@RequestMapping(method = RequestMethod.POST, value = "/GetCoverPhoto")
+	public File[] getCoverPhoto(@RequestParam(value = "id") int id, @RequestParam(value = "photoName") String photoName) {
 		if (topicRepository.existsById(id)) {
-			
 			Topic t = topicRepository.findById(id).get();
-			t.addCoverPhotos(tcps.storeFile(file, t));
-			return topicRepository.save(t);
-		}
-		return null;
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/AddCoverPhotoByName")
-	public Topic addCoverPhotoByName(@RequestParam(value="file") MultipartFile file, @RequestParam(value="name") String name) {
-		if (topicRepository.existsByName(name)) {
-			Topic t = topicRepository.findByName(name);
-			t.addCoverPhotos(tcps.storeFile(file, t));
-			return topicRepository.save(t);
-		}
-		return null;
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/AddCoverPhotos")
-	public Topic addCoverPhotos(@RequestParam(value="files")  MultipartFile[] files, @RequestParam(value="id") int id) {
-		if (topicRepository.existsById(id)) {
+			String topicName = t.getName();
+			String downloadPath = "images/topicImages/";
 			
-			Topic t = topicRepository.findById(id).get();
-			List<TopicCoverPhoto> tcp = tcps.storeMultipleFile(files, t);
-			t.addMCoverPhotos(tcp);
-			return topicRepository.saveAndFlush(t);
+			try {
+				String realdownloadPath = getClass().getClassLoader().getResource(downloadPath).getPath();
+				File dir = new File(realdownloadPath + topicName + "/");
+				if (dir.exists()) {
+					File [] files = dir.listFiles(new FilenameFilter() {
+					    @Override
+					    public boolean accept(File dir, String name) {
+					        return name.startsWith(topicName + "_" + photoName + ".");
+					    }
+					});
+					return files;
+				} else {
+					System.out.println("Topic photo folder not found.");
+					return null;
+				}
+			} catch (Exception e) {
+				System.out.println("topicImages folder not found.");
+				return null;
+			}
+		} else {
+			System.out.println("Topic not found.");
+			return null;
 		}
-		return null;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/AddCoverPhotosByName")
-	public Topic addCoverPhotosByName(@RequestParam(value="files", required=true) MultipartFile[] files, @RequestParam(value="name") String name) {
-		if (topicRepository.existsByName(name)) {
-			Topic t = topicRepository.findByName(name);
-			List<TopicCoverPhoto> tcp = tcps.storeMultipleFile(files, t);
-			t.addMCoverPhotos(tcp);
-			return topicRepository.saveAndFlush(t);
-		}
-		return null;
- 	}
-	@Transactional
-	@RequestMapping(method = RequestMethod.POST, value = "/DeleteCoverPhoto")
-	public Topic deleteCoverPhoto(@RequestParam(value="photoId") String photoID, @RequestParam(value="id") int id) {
+	@RequestMapping(method = RequestMethod.POST, value = "/GetThumCoverPhoto")
+	public File[] getThumCoverPhoto(@RequestParam(value = "id") int id, @RequestParam(value = "photoName") String photoName) {
 		if (topicRepository.existsById(id)) {
-			boolean result = tcps.deleteFile(photoID);
-			if (result == true) {
-				return topicRepository.findById(id).get();
+			Topic t = topicRepository.findById(id).get();
+			String topicName = t.getName();
+			String downloadPath = "images/topicImages/";
+			
+			try {
+				String realdownloadPath = getClass().getClassLoader().getResource(downloadPath).getPath();
+				File dir = new File(realdownloadPath + topicName + "/");
+				if (dir.exists()) {
+					File [] files = dir.listFiles(new FilenameFilter() {
+					    @Override
+					    public boolean accept(File dir, String name) {
+					        return name.startsWith(topicName + "_" + "thum_" + photoName + ".");
+					    }
+					});
+					return files;
+				} else {
+					System.out.println("Topic photo folder not found.");
+					return null;
+				}
+			} catch (Exception e) {
+				System.out.println("topicImages folder not found.");
+				return null;
 			}
-		} 
-		return null;
-	}
-	@Transactional
-	@RequestMapping(method = RequestMethod.POST, value = "/DeleteCoverPhotoByNames")
-	public Topic deleteCoverPhotosByNames(@RequestParam(value="fileName") String fileName, @RequestParam(value="name") String name) {
-		if (topicRepository.existsByName(name)) {
-			boolean result = tcps.deleteFileByFileName(fileName);
-			if (result == true) {
-				return topicRepository.findByName(name);
-			}
+		} else {
+			System.out.println("Topic not found.");
+			return null;
 		}
-		return null;
-	}*/
+	}
 	
+	@RequestMapping(method = RequestMethod.POST, value = "/GetAllNorCoverPhotos")
+	public File[] getAllNorCoverPhotos(@RequestParam(value = "id") int id) {
+		if (topicRepository.existsById(id)) {
+			Topic t = topicRepository.findById(id).get();
+			String topicName = t.getName();
+			String downloadPath = "images/topicImages/";
+			
+			try {
+				String realdownloadPath = getClass().getClassLoader().getResource(downloadPath).getPath();
+				File dir = new File(realdownloadPath + topicName + "/");
+				if (dir.exists()) {
+					File [] files = dir.listFiles(new FilenameFilter() {
+					    @Override
+					    public boolean accept(File dir, String name) {
+					        return !name.contains("_thum_");
+					    }
+					});
+					return files;
+				} else {
+					System.out.println("Topic photo folder not found.");
+					return null;
+				}
+			} catch (Exception e) {
+				System.out.println("topicImages folder not found.");
+				return null;
+			}
+		} else {
+			System.out.println("Topic not found.");
+			return null;
+		}
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/GetAllThumCoverPhotos")
+	public File[] getAllThumCoverPhotos(@RequestParam(value = "id") int id) {
+		if (topicRepository.existsById(id)) {
+			Topic t = topicRepository.findById(id).get();
+			String topicName = t.getName();
+			String downloadPath = "images/topicImages/";
+			
+			try {
+				String realdownloadPath = getClass().getClassLoader().getResource(downloadPath).getPath();
+				File dir = new File(realdownloadPath + topicName + "/");
+				if (dir.exists()) {
+					File [] files = dir.listFiles(new FilenameFilter() {
+					    @Override
+					    public boolean accept(File dir, String name) {
+					        return name.contains("_thum_");
+					    }
+					});
+					return files;
+				} else {
+					System.out.println("Topic photo folder not found.");
+					return null;
+				}
+			} catch (Exception e) {
+				System.out.println("topicImages folder not found.");
+				return null;
+			}
+			
+		} else {
+			System.out.println("Topic not found.");
+			return null;
+		}
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/GetAllCoverPhotos")
+	public File[] getAllCoverPhotos(@RequestParam(value = "id") int id) {
+		if (topicRepository.existsById(id)) {
+			Topic t = topicRepository.findById(id).get();
+			String topicName = t.getName();
+			String downloadPath = "images/topicImages/";
+			try {
+				String realdownloadPath = getClass().getClassLoader().getResource(downloadPath).getPath();
+				
+				File dir = new File(realdownloadPath + topicName + "/");
+				if (dir.exists()) {
+					return dir.listFiles();	
+				} else {
+					System.out.println("Topic photo folder not found.");
+					return null;
+				}
+			} catch (Exception e) {
+				System.out.println("topicImages folder not found");
+				return null;
+			}
+
+		} else {
+			System.out.println("Topic not found.");
+			return null;
+		}
+	}
 	
 
-	
 
 }
